e53c1fe3a3904d3dcf873cf1e65b1ea51f421515
exoduszin <exoduszin@gmail.com>
2022-07-17 01:01:56 -0300

    init

---
+++ src/index.js
@@ -0,0 +1,6 @@
+const { Client, Intents } = require('discord.js')
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] })
+
+client.on('ready', () => console.log(client.user))
+
+client.login()
